917b8792ce4343caac6f80a49e2660a6b49b36e1
rahulawasthi <rahul.awasthi@nagarro.com>
2023-06-17 01:42:20 +0530

    Adding configuration to create migration according to need

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -18,5 +19,11 @@ namespace Infrastructure.Data
         public DbSet<ProductBrand> ProductBrands { get; set; } = default!;
 
         public DbSet<ProductType> ProductTypes { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
