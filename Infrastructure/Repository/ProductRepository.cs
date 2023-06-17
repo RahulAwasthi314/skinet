b56b6cc634247bd9c627ddb5afb2f1b8289b5777
rahulawasthi <rahul.awasthi@nagarro.com>
2023-06-17 13:34:01 +0530

    Corrected the Brand and Type response in API, while getting the Product response
    Added the Query chaining\nChanged QueryFunction to FindFirstOrDefaultAsync to support query chaining

--- Infrastructure/Repository/ProductRepository.cs
+++ Infrastructure/Repository/ProductRepository.cs
@@ -31,12 +31,19 @@ namespace Infrastructure.Repository
 
         public async Task<Product?> GetProductByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
+                // Replace FindAsync to SingleOrDefaultAsync or FirstOrDefaultAsync
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
         }
 
         public async Task<ProductType?> GetProductTypeByIdAsync(int id)
